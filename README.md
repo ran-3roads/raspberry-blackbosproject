7f8e06661245ba9dceea3167e91fa07c452519ca
ran-3roads <ckstjd9735@gmail.com>
2020-11-26 20:32:25 +0900

    2nd commit

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-circuit.py= 라즈베리파이에서 초음파 받아서 mqtt.py로 보냄
+1 commit: circuit.py= 라즈베리파이에서 초음파 받아서 mqtt.py로 보냄
 mqtt.py= circuit.py 에서 받은초음파 정보를 계산함(움직임 발생시 사진캡쳐->동영상으로 바꿔야함)
 project.html= 웹서버 인덱스
 mqttio6.js= 메시지 js
+
+2 commit: mqtt.py 사진에서 동영상 녹화기능 추가.
--- park-mqtt.py
+++ park-mqtt.py
@@ -1,18 +1,14 @@
+# publisher
 
 import time
 import paho.mqtt.client as mqtt
 import circuit # 초음파 센서 입력 모듈 임포트
 import picamera
 import datetime
-import os
-import io
-import cv2
-import numpy as np
+
 
 camera = picamera.PiCamera()
-fileName = ""
-stream = io.BytesIO()
-
+filename=""
 broker_ip = "localhost" # 현재 이 컴퓨터를 브로커로 설정
 
 client = mqtt.Client()
@@ -40,18 +36,14 @@ while(True):
         else:
             b=distance
             if compare(a,b)==True:
-                if len(fileName) != 0:
-                    os.unlink(fileName)
-                stream.seek(0) # 파일 포인터를 스트림 맨 앞으로 위치시킴. 이곳에
-서부터 이미지 데이터 저장
-                stream.truncate()
-                time.sleep(1)
-                camera.capture(stream, format='jpeg', use_video_port=True)
-                data = np.frombuffer(stream.getvalue(), dtype=np.uint8)
-                image = cv2.imdecode(data, 1)
-                takeTime = time.time()
-                fileName = "./static/%d.jpg" % (takeTime * 10)
-                cv2.imwrite(fileName, image)
+                filename=datetime.datetime.now().strftime("%Y-%-m-%-d %H-%M-%S")
+                camera.resolution = (800, 600)
+                camera.start_preview()
+                camera.start_recording('./static/'+filename+'.h264')
+                camera.wait_recording(10)
+                camera.capture('./static/'+filename+'.jpg', use_video_port=True)
+                camera.wait_recording(3)
+                camera.stop_recording()
 
 client.loop_stop()
 client.disconnect()
